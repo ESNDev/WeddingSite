ced083ab14645fd9f478257340d52f18ce18ad0c
Devon Harmon <devonleeh@gmail.com>
2024-09-26 13:21:44 -0400

    z

--- jscontrol.js
+++ jscontrol.js
@@ -7,3 +7,24 @@ function decorload(){
 function tabledecorload(){
     window.location.href="./tabledecor.html";
 }
+function archesload(){
+    window.location.href="./arches.html";
+}
+function signsload(){
+    window.location.href="./signs.html";
+}
+function tablesload(){
+    window.location.href="./tables.html";
+}
+function linensload(){
+    window.location.href="./linens.html";
+}
+function aboutusload(){
+    window.location.href="./aboutme.html";
+}
+function galleryload(){
+    window.location.href="./gallery.html";
+}
+function contactload(){
+    
+}
