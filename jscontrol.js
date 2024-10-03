7893a8fa3b1506f523b51ed80bc3445745453608
Devon <devonleeh@gmail.com>
2024-10-02 20:53:53 -0400

    z

--- jscontrol.js
+++ jscontrol.js
@@ -34,9 +34,18 @@ function loaddecoritems(){
     drapery2();
     drapery3();
     drapery4();
+    candle1();
+}
+
+function loadtabledecoritems(){
     vases1();
     vases2();
-    candle1();
+    vases3();
+    vases4();
+    vases5();
+    vases6();
+    vases7();
+    vases8();
 }
 
 function itemList(){
@@ -76,6 +85,42 @@ function itemList(){
         vases2img = document.createElement("img");
         vases2img.src = "example.png";
         itemlistelement.appendChild(vases2img);
+    }
+    if(localStorage.getItem("vases3Selected") == "t")
+    {
+        vases3img = document.createElement("img");
+        vases3img.src = "example.png";
+        itemlistelement.appendChild(vases3img);
+    }
+    if(localStorage.getItem("vases4Selected") == "t")
+    {
+        vases4img = document.createElement("img");
+        vases4img.src = "example.png";
+        itemlistelement.appendChild(vases4img);
+    }
+    if(localStorage.getItem("vases5Selected") == "t")
+    {
+        vases5img = document.createElement("img");
+        vases5img.src = "example.png";
+        itemlistelement.appendChild(vases5img);
+    }
+    if(localStorage.getItem("vases6Selected") == "t")
+    {
+        vases6img = document.createElement("img");
+        vases6img.src = "example.png";
+        itemlistelement.appendChild(vases6img);
+    }
+    if(localStorage.getItem("vases7Selected") == "t")
+    {
+        vases7img = document.createElement("img");
+        vases7img.src = "example.png";
+        itemlistelement.appendChild(vases7img);
+    }
+    if(localStorage.getItem("vases8Selected") == "t")
+    {
+        vases8img = document.createElement("img");
+        vases8img.src = "example.png";
+        itemlistelement.appendChild(vases8img);
     }
     if(localStorage.getItem("candle1Selected") == "t")
     {
@@ -232,6 +277,156 @@ function vases2(){
     {
         vases2element.style.color = "#648396";
         vases2element.innerText = "+";
+    }
+}
+
+function vases3clicked(){
+    if(localStorage.getItem("vases3Selected") == "t")
+    {
+        localStorage.setItem("vases3Selected", "f");
+    }
+    else
+    {
+        localStorage.setItem("vases3Selected", "t");
+    }
+    vases3();
+}
+function vases3(){
+    vases3element = document.getElementById("vases3");
+    if(localStorage.getItem("vases3Selected") == "t")
+    {
+        vases3element.style.color = "Red";
+        vases3element.innerText = "X";
+    }
+    else
+    {
+        vases3element.style.color = "#648396";
+        vases3element.innerText = "+";
+    }
+}
+
+function vases4clicked(){
+    if(localStorage.getItem("vases4Selected") == "t")
+    {
+        localStorage.setItem("vases4Selected", "f");
+    }
+    else
+    {
+        localStorage.setItem("vases4Selected", "t");
+    }
+    vases4();
+}
+function vases4(){
+    vases4element = document.getElementById("vases4");
+    if(localStorage.getItem("vases4Selected") == "t")
+    {
+        vases4element.style.color = "Red";
+        vases4element.innerText = "X";
+    }
+    else
+    {
+        vases4element.style.color = "#648396";
+        vases4element.innerText = "+";
+    }
+}
+
+function vases5clicked(){
+    if(localStorage.getItem("vases5Selected") == "t")
+    {
+        localStorage.setItem("vases5Selected", "f");
+    }
+    else
+    {
+        localStorage.setItem("vases5Selected", "t");
+    }
+    vases5();
+}
+function vases5(){
+    vases5element = document.getElementById("vases5");
+    if(localStorage.getItem("vases5Selected") == "t")
+    {
+        vases5element.style.color = "Red";
+        vases5element.innerText = "X";
+    }
+    else
+    {
+        vases5element.style.color = "#648396";
+        vases5element.innerText = "+";
+    }
+}
+
+function vases6clicked(){
+    if(localStorage.getItem("vases6Selected") == "t")
+    {
+        localStorage.setItem("vases6Selected", "f");
+    }
+    else
+    {
+        localStorage.setItem("vases6Selected", "t");
+    }
+    vases6();
+}
+function vases6(){
+    vases6element = document.getElementById("vases6");
+    if(localStorage.getItem("vases6Selected") == "t")
+    {
+        vases6element.style.color = "Red";
+        vases6element.innerText = "X";
+    }
+    else
+    {
+        vases6element.style.color = "#648396";
+        vases6element.innerText = "+";
+    }
+}
+
+function vases7clicked(){
+    if(localStorage.getItem("vases7Selected") == "t")
+    {
+        localStorage.setItem("vases7Selected", "f");
+    }
+    else
+    {
+        localStorage.setItem("vases7Selected", "t");
+    }
+    vases7();
+}
+function vases7(){
+    vases7element = document.getElementById("vases7");
+    if(localStorage.getItem("vases7Selected") == "t")
+    {
+        vases7element.style.color = "Red";
+        vases7element.innerText = "X";
+    }
+    else
+    {
+        vases7element.style.color = "#648396";
+        vases7element.innerText = "+";
+    }
+}
+
+function vases8clicked(){
+    if(localStorage.getItem("vases8Selected") == "t")
+    {
+        localStorage.setItem("vases8Selected", "f");
+    }
+    else
+    {
+        localStorage.setItem("vases8Selected", "t");
+    }
+    vases8();
+}
+function vases8(){
+    vases8element = document.getElementById("vases8");
+    if(localStorage.getItem("vases8Selected") == "t")
+    {
+        vases8element.style.color = "Red";
+        vases8element.innerText = "X";
+    }
+    else
+    {
+        vases8element.style.color = "#648396";
+        vases8element.innerText = "+";
     }
 }
 
