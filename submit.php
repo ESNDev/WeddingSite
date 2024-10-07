604f157bc8cdbe86f4bf3c66020819c73caa0137
Devon Harmon <devonleeh@gmail.com>
2024-10-06 23:11:55 -0400

    z

--- submit.php
+++ submit.php
@@ -1,4 +1,4 @@
 <?php
-$msg = _get["name"];
+$msg = _get["name"] . _get["phone"] . _get["email"] . _get["location"] . _get["guests"] . _get["message"];
 mail("devonleeh@gmail.com","Subject",$msg);
 ?>
